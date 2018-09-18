6f32dbba6dfea37e527ab8be032f670c26d0084c
johnanosky <john@nosky.tech>
2018-09-17 21:41:24 -0400

    Whitespace management adhoc.sh

--- adhoc.sh
+++ adhoc.sh
@@ -10,35 +10,35 @@
 LOCAL_ADDR=`ip addr | grep 192 | awk -F '.' '{print $4}' | awk -F '/' '{print $1}'`
 W_DEV=`iw dev | grep -i interface | awk -F ' ' '{print $2}'`
 case $1 in
-        open)
-				ip link set $W_DEV down
-				killall NetworkManager >/dev/null 2>&1
-				killall wpa_supplicant >/dev/null 2>&1
-				ip link set $W_DEV up
-                wpa_supplicant -i $W_DEV -Dnl80211 -c open_adhoc.conf &
-                ;;
-        wep)
-				ip link set $W_DEV down
-				killall NetworkManager >/dev/null 2>&1
-				killall wpa_supplicant >/dev/null 2>&1
-				ip link set $W_DEV up
-                wpa_supplicant -i $W_DEV -Dnl80211 -c wep_adhoc.conf &
-                ;;
-        *)
-                echo "Usage: $0 [open|wep] [local 4th octect] [remote 4th octect]"
-				exit 0
+	open)
+		ip link set $W_DEV down
+		killall NetworkManager >/dev/null 2>&1
+		killall wpa_supplicant >/dev/null 2>&1
+		ip link set $W_DEV up
+		wpa_supplicant -i $W_DEV -Dnl80211 -c open_adhoc.conf &
+		;;
+	wep)
+		ip link set $W_DEV down
+		killall NetworkManager >/dev/null 2>&1
+		killall wpa_supplicant >/dev/null 2>&1
+		ip link set $W_DEV up
+		wpa_supplicant -i $W_DEV -Dnl80211 -c wep_adhoc.conf &
+		;;
+		*)
+		echo "Usage: $0 [open|wep] [local 4th octect] [remote 4th octect]"
+		exit 0
 esac
 sleep 10
 iw $W_DEV link
 if [ -z $LOCAL_ADDR ]
 then
-        ip addr add 192.168.1.$2/24 dev $W_DEV
-        ip route add 192.168.1.0/24 dev $W_DEV
-		ip route add default via 192.168.1.254
+	ip addr add 192.168.1.$2/24 dev $W_DEV
+	ip route add 192.168.1.0/24 dev $W_DEV
+	ip route add default via 192.168.1.254
 else
-        if [ $LOCAL_ADDR != $2 ]
-        then
-                ip addr del 192.168.1.$LOCAL_ADDR/24 dev $W_DEV
-        fi
+	if [ $LOCAL_ADDR != $2 ]
+	then
+		ip addr del 192.168.1.$LOCAL_ADDR/24 dev $W_DEV
+	fi
 fi
 ping -c 10 192.168.1.$3 &
