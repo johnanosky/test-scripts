568a297df481c2ef6edc508eb02351b065c66983
johnanosky <john@nosky.tech>
2018-09-26 09:24:21 -0400

    Added logic for unknown TTY interface and removed set -x

--- console.sh
+++ console.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -x
 # A quick script to automate minicom when using RS-232 to USB adapters.
 
 if [ -z $2 ]
@@ -15,26 +14,34 @@ case $1 in
 		# Grep dmesg output for the last enumerated USB TTY interface
 		# Useful when you plug in a new adapter and don't know the current iteration
 		TTY_DEV=`dmesg | grep ttyUSB | tail -n1 | awk -F'ttyUSB' '{print $2}'`
-		TTY_TEST=`ps -aux | grep minicom | grep ttyUSB$TTY_DEV | tail -n1`
+		if [ -z $TTY_DEV ]; then
+			echo "No suitable interface found, please plug in device and try again."
+			exit 1
+		fi
+		TTY_TEST=`ps -aux | grep minicom | grep ttyUSB$TTY_DEV | tail -n1 | awk -F " " '{print $2}'`
 		if [ -z $TTY_TEST ]; then
 			minicom -c on -D /dev/ttyUSB$TTY_DEV -8 -o -l -b $C_BAUD
-		else echo "A minicom session is already running on /dev/ttyUSB$TTY_DEV"
+		else echo "A minicom session with PID $TTY_TEST is already running on /dev/ttyUSB$TTY_DEV"
 		fi
 		;;
 	imx7)
 		# For NXP i.MX7 SABRE EVK
 		TTY_DEV=`dmesg | grep ttyUSB | tail -n2 | head -n1 | awk -F'ttyUSB' '{print $2}'`
-		TTY_TEST=`ps -aux | grep minicom | grep ttyUSB$TTY_DEV | tail -n1`
+		if [ -z $TTY_DEV ]; then
+			echo "No suitable interface found, please plug in device and try again."
+			exit 1
+		fi
+		TTY_TEST=`ps -aux | grep minicom | grep ttyUSB$TTY_DEV | tail -n1 | awk -F " " '{print $2}'`
 		if [ -z $TTY_TEST ]; then
 			minicom -c on -D /dev/ttyUSB$TTY_DEV -8 -o -l -b $C_BAUD
-		else echo "A minicom session is already running on /dev/ttyUSB$TTY_DEV"
+		else echo "A minicom session with PID $TTY_TEST is already running on /dev/ttyUSB$TTY_DEV"
 		fi
 		;;
 	[0-9])
-		TTY_TEST=`ps -aux | grep minicom | grep ttyUSB$1`
+		TTY_TEST=`ps -aux | grep minicom | grep ttyUSB$1 | awk -F " " '{print $2}'`
 		if [ -z $TTY_TEST ]; then
 			minicom -c on -D /dev/ttyUSB$1 -8 -o -l -b $C_BAUD
-		else echo "A minicom session is already running on /dev/ttyUSB$1"
+		else echo "A minicom session with PID $TTY_TEST is already running on /dev/ttyUSB$1"
 		fi
 		;;
 	*)
